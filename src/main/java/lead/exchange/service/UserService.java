313cafe0137dd18d008c94baff973c7126ebf46e
vladimir <vbrezhnev@intetech.ru>
2025-11-11 02:39:14 +0300

    IamVeryVeryVeryVeryVeryLoveLinterInThisProject

--- src/main/java/lead/exchange/service/UserService.java
+++ src/main/java/lead/exchange/service/UserService.java
@@ -10,21 +10,22 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
     private final UserRepository userRepository;
+
     @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     public User getUserByTelegramId(String telegramId) {
-        return userRepository.findByTelegramId(telegramId).
-                orElseThrow(() -> new ResourceNotFoundException(
+        return userRepository.findByTelegramId(telegramId)
+                .orElseThrow(() -> new ResourceNotFoundException(
                         "User not found with telegramId: " + telegramId
                 ));
     }
 
     public void checkUserExistByUserid(UUID userId) {
-        userRepository.findByUserId(userId).
-                orElseThrow(() -> new ResourceNotFoundException(
+        userRepository.findByUserId(userId)
+                .orElseThrow(() -> new ResourceNotFoundException(
                         "User not found with userId: " + userId
                 ));
     }
