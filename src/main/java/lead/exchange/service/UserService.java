cc5146d70ab589d1f9f92f0f4baccb64ea3adfb8
vladimir <vbrezhnev@intetech.ru>
2025-11-11 13:27:41 +0300

    EditUserServiceAutowired

--- src/main/java/lead/exchange/service/UserService.java
+++ src/main/java/lead/exchange/service/UserService.java
@@ -4,17 +4,14 @@ import java.util.UUID;
 import lead.exchange.entity.User;
 import lead.exchange.exception.ResourceNotFoundException;
 import lead.exchange.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
-
-    @Autowired
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
 
     public User getUserByTelegramId(String telegramId) {
         return userRepository.findByTelegramId(telegramId)
