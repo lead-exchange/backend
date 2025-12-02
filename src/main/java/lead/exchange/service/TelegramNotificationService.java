8a7a1ac58c57a58a283ef265f410cd2fbf86240b
a.s.lushnikova <a.s.lushnikova@tbank.ru>
2025-12-02 22:07:26 +0300

    feature(fixes): fix

--- src/main/java/lead/exchange/service/TelegramNotificationService.java
+++ src/main/java/lead/exchange/service/TelegramNotificationService.java
@@ -6,13 +6,12 @@ import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.model.request.KeyboardButton;
 import com.pengrad.telegrambot.model.request.ReplyKeyboardMarkup;
 import com.pengrad.telegrambot.request.SendMessage;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
@@ -58,22 +57,22 @@ public class TelegramNotificationService {
 
         @Override
         public int process(List<Update> updates) {
-            for (Update update : updates) {
-                if (update.message() != null && "/start".equals(update.message().text())) {
-                    requestPhoneShare(update.message().chat().id());
-                }
-
-                if (update.message() != null && update.message().contact() != null) {
-                    String phone = update.message().contact().phoneNumber();
-                    Long chatId = update.message().chat().id();
-                    String telegramId = update.message().from().username();
-
-                    userService.savePhoneAndChat(telegramId, chatId, phone);
-
-                    SendMessage msg = new SendMessage(chatId, "Спасибо! Телефон сохранён 📱");
-                    telegramBot.execute(msg);
-                }
-            }
+//            for (Update update : updates) {
+//                if (update.message() != null && "/start".equals(update.message().text())) {
+//                    requestPhoneShare(update.message().chat().id());
+//                }
+//
+//                if (update.message() != null && update.message().contact() != null) {
+//                    String phone = update.message().contact().phoneNumber();
+//                    Long chatId = update.message().chat().id();
+//                    String telegramId = update.message().from().username();
+//
+//                    userService.savePhoneAndChat(telegramId, chatId, phone);
+//
+//                    SendMessage msg = new SendMessage(chatId, "Спасибо! Телефон сохранён 📱");
+//                    telegramBot.execute(msg);
+//                }
+//            }
             return UpdatesListener.CONFIRMED_UPDATES_ALL;
         }
     }
