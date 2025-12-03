5cfeafb4097860ff6549d4d3cee253da4d5b72dd
Maria Barkovskaya <barkovskaya.maria@mail.ru>
2025-12-03 20:03:33 +0300

    fix tests

--- src/test/java/lead/exchange/controller/MatchControllerTest.java
+++ src/test/java/lead/exchange/controller/MatchControllerTest.java
@@ -17,8 +17,13 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-
+@ActiveProfiles("test")
+@SpringBootTest
+@AutoConfigureMockMvc
 public class MatchControllerTest extends IntegrationTest {
 
     private UUID testLeadId;
