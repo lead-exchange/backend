0d6970f13e85323b8abc88141bc1f47aea947bdd
a.s.lushnikova <a.s.lushnikova@tbank.ru>
2025-12-08 21:09:24 +0300

    feat: fix tests

--- src/test/java/lead/exchange/repository/MatchRepositoryTest.java
+++ src/test/java/lead/exchange/repository/MatchRepositoryTest.java
@@ -1,5 +1,12 @@
 package lead.exchange.repository;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import java.util.Optional;
 import lead.exchange.IntegrationTest;
 import lead.exchange.entity.Estate;
 import lead.exchange.entity.Lead;
@@ -14,12 +21,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
-import java.util.List;
-import java.util.Optional;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class MatchRepositoryTest extends IntegrationTest {
